5285e863c920807012a7f993720e24bb68bec544
Laura <veggie_munchiesandcrunchies@hotmail.com>
2019-04-03 22:34:21 -0400

    buttons added to look book

--- client/src/components/lookbook.js
+++ client/src/components/lookbook.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Card } from 'antd';
+import { Row, Col, Card, Button } from 'antd';
 import 'antd/dist/antd.css';
 import "./lookbook.css";
 
@@ -13,33 +13,54 @@ import HairPic7 from "./hairsquare7.jpg";
 function LookBook() {
     return (
         <div>
-            <Row gutter={16}>
+            <Row>
+                <Col span={24}>
+                    <h1 id="header">Look Book</h1>
+                </Col>
+            </Row>
+            <Row gutter={20}>
                 <Col span={6}>
-                    <Card 
+                    <Card
                         hoverable
-                        cover={<img alt="example" className="pic" src={HairPic4} />} 
+                        cover={<img alt="example" className="pic" src={HairPic4} />}
                         bordered={false}>
+                        <div id="buttonbackground">
+                            <Button className="buttons commentButton" justify="center" type="default">Add Comment</Button>
+                            <Button className="buttons" justify="center" type="danger">Delete</Button>
+                        </div>
                     </Card>
                 </Col>
                 <Col span={6}>
-                <Card 
+                    <Card
                         hoverable
-                        cover={<img alt="example" className="pic" src={HairPic5} />} 
+                        cover={<img alt="example" className="pic" src={HairPic5} />}
                         bordered={false}>
+                        <div id="buttonbackground">
+                            <Button className="buttons commentButton" justify="center" type="default">Add Comment</Button>
+                            <Button className="buttons" justify="center" type="danger">Delete</Button>
+                        </div>
                     </Card>
                 </Col>
                 <Col span={6}>
-                <Card 
+                    <Card
                         hoverable
-                        cover={<img alt="example" className="pic" src={HairPic6} />} 
+                        cover={<img alt="example" className="pic" src={HairPic6} />}
                         bordered={false}>
+                        <div id="buttonbackground">
+                            <Button className="buttons commentButton" justify="center" type="default">Add Comment</Button>
+                            <Button className="buttons" justify="center" type="danger">Delete</Button>
+                        </div>
                     </Card>
                 </Col>
                 <Col span={6}>
-                <Card 
+                    <Card
                         hoverable
-                        cover={<img alt="example" className="pic" src={HairPic7} />} 
+                        cover={<img alt="example" className="pic" src={HairPic7} />}
                         bordered={false}>
+                        <div id="buttonbackground">
+                            <Button className="buttons commentButton" justify="center" type="default">Add Comment</Button>
+                            <Button className="buttons" justify="center" type="danger">Delete</Button>
+                        </div>
                     </Card>
                 </Col>
             </Row>
