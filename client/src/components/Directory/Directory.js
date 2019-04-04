6a95215fdb0a2d21f61110c1337b56dcc0f75619
Laura <veggie_munchiesandcrunchies@hotmail.com>
2019-04-04 19:07:42 -0400

    on click for show works in directory

--- client/src/components/Directory/Directory.js
+++ client/src/components/Directory/Directory.js
@@ -2,17 +2,34 @@ import React from "react";
 import { Button } from 'antd';
 import '../Directory/Directory.css';
 
+class Directory extends React.Component {
+    showBook() {
+        console.log("showBook works")
+    }
 
-const Directory = () => (
-    <div id="container">
-         <Button type="dashed" block id="">All Appointments</Button>
-         <Button type="dashed" block>My Schedule</Button>
-         <Button type="dashed" block>Lookbook</Button>
-         <Button type="dashed" block>Photos</Button>
-         <Button type="dashed" block>Users</Button>
-         <Button type="dashed" block>Inventory</Button>
-    </div>
+    render() {
+        return (
+            <div id="container">
+                <Button type="dashed" block id="">All Appointments</Button>
+                <Button type="dashed" block>My Schedule</Button>
+                <Button type="dashed" block onClick={this.showBook}>Lookbook</Button>
+                <Button type="dashed" block>Photos</Button>
+                <Button type="dashed" block>Users</Button>
+                <Button type="dashed" block>Inventory</Button>
+            </div> 
+        )
+    }
+}
+// const Directory = () => (
+//     <div id="container">
+//          <Button type="dashed" block id="">All Appointments</Button>
+//          <Button type="dashed" block>My Schedule</Button>
+//          <Button type="dashed" block onClick={() => props.shuffle()}>Lookbook</Button>
+//          <Button type="dashed" block>Photos</Button>
+//          <Button type="dashed" block>Users</Button>
+//          <Button type="dashed" block>Inventory</Button>
+//     </div>
         
-);
+// );
 
 export default Directory;
