8b670e569bb1edcb587e2b6ae537c2dcf03c007b
Laura <veggie_munchiesandcrunchies@hotmail.com>
2019-04-04 20:15:46 -0400

    working on on click

--- client/src/components/Directory/Directory.js
+++ client/src/components/Directory/Directory.js
@@ -3,12 +3,17 @@ import { Button } from 'antd';
 import '../Directory/Directory.css';
 
 class Directory extends React.Component {
-    showBook() {
-        console.log("showBook works")
-    }
+
+    // showBook() {
+    //     console.log("showBook works")
+    //     this.setState({
+    //         showComponent: true,
+    //       });
+    // }
 
     render() {
         return (
+            
             <div id="container">
                 <Button type="dashed" block id="">All Appointments</Button>
                 <Button type="dashed" block>My Schedule</Button>
@@ -20,16 +25,5 @@ class Directory extends React.Component {
         )
     }
 }
-// const Directory = () => (
-//     <div id="container">
-//          <Button type="dashed" block id="">All Appointments</Button>
-//          <Button type="dashed" block>My Schedule</Button>
-//          <Button type="dashed" block onClick={() => props.shuffle()}>Lookbook</Button>
-//          <Button type="dashed" block>Photos</Button>
-//          <Button type="dashed" block>Users</Button>
-//          <Button type="dashed" block>Inventory</Button>
-//     </div>
-        
-// );
 
 export default Directory;
