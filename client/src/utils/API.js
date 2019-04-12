45a24bda8b427aa8cbe5d2d2d55d28d03ce2521c
MistaWizard <tylermckellar@gmail.com>
2019-04-11 21:09:06 -0400

    More get routes defined in React

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,8 +4,14 @@ export default {
     saveIt: function(imageData) {
         return axios.post("/api/images", imageData);
     },
+    getImages: function() {
+        return axios.get("/api/images");
+    },
     saveTheDate: function(apptData) {
         return axios.post("/api/appt", apptData);
+    },
+    getTheDate: function() {
+        return axios.get("/api/appt");
     },
     saveTheUser: function(userData) {
         return axios.post("/api/user", userData);
