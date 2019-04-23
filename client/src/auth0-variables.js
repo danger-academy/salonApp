a38697618e71a5be61998a7e722da9d1fc1d2d51
Maniccia <mariamaniccia@gmail.com>
2019-04-23 18:16:42 -0400

    callback switch

--- client/src/auth0-variables.js
+++ client/src/auth0-variables.js
@@ -2,8 +2,8 @@ export const AUTH_CONFIG = {
   domain: "salon-app.auth0.com",
   roleUrl: "https://salon-app/role",
   clientId: "pBVScNgayX1de74KvvmgYywQU0ti2fqt",
-  callbackUrl:  "http://localhost:3000/callback"
-  // "https://ashley-love.herokuapp.com/callback"
+  // callbackUrl:  "http://localhost:3000/callback"
+  callbackUrl: "https://ashley-love.herokuapp.com/callback"
  
 };
 export default AUTH_CONFIG;
